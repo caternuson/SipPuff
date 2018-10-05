97792fcc77b943b9501ca81856f047db63d28c6e
caternuson <methuzla@speakeasy.net>
2018-10-04 20:32:53 -0700

    update morse to work on edges

--- sipnpuff_morse.py
+++ sipnpuff_morse.py
@@ -7,7 +7,7 @@ mpr = adafruit_mprls.MPRLS(i2c)
 
 THRESH_LOW    = 10 # delta in hPa
 THRESH_HIGH   = 10 # delta in hPa
-DEBOUNCE = 0.005
+CHAR_PAUSE = 1
 
 def pressure_sensor_init(count=10, delay=0.1):
     reading = 0
@@ -18,6 +18,9 @@ def pressure_sensor_init(count=10, delay=0.1):
     return reading - THRESH_LOW, reading + THRESH_HIGH
 
 sip_threshold , puff_threshold = pressure_sensor_init()
+last_blip = time.monotonic()
+char_start = False
+char = ""
 
 while True:
     # driver checks conversion ready status, so OK do run this as fast as needed
@@ -25,10 +28,27 @@ while True:
 
     # PUFF = dit
     if pressure > puff_threshold:
-        print("dit ", end='')
-        time.sleep(DEBOUNCE)
+        last_blip = time.monotonic()
+        char += "."
+        char_start = True
+        #print(".", end='')
+        # park here until pressure goes back down
+        while pressure > puff_threshold:
+            pressure = mpr.pressure
 
     # SIP = dah
     if pressure < sip_threshold:
-        print("dah ", end='')
-        time.sleep(DEBOUNCE)
+        last_blip = time.monotonic()
+        char += "-"
+        char_start = True
+        #print("-", end='')
+        # park here until pressure goes back up
+        while pressure < sip_threshold:
+            pressure = mpr.pressure
+
+    if char_start and time.monotonic() - last_blip > CHAR_PAUSE:
+        char_start = False
+        print(char+"   ", end='')
+        char = ""
+        # let do a quick re-cal while we're here...
+        sip_threshold , puff_threshold = pressure_sensor_init(5, 0.01)
